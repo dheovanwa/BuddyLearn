89217a52dee221bbfc18ac999e49bba5738ab451
KepinTheNoob <kevinsetiawan2412@gmail.com>
2025-04-12 13:59:35 +0700

    Login UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,5 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.9")
     implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
 }
